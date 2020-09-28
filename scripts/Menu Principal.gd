e9abdbc805226b78763d77cc52bd592ee4de25f0
Nyveon <54124297+Nyveon@users.noreply.github.com>
2020-09-28 11:15:07 -0300

    Menu principal
    -Titulo
    -Boton de entrar
    -Boton de salir
    Usando un font temporary, sujeto a cambio
    (todo segun tutorial de las clases)

---
+++ scripts/Menu Principal.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Panel/CenterContainer/VBoxContainer/Start.connect("pressed", self, "on_Start_pressed")  # Señales del boton de start 
+	$Panel/CenterContainer/VBoxContainer/Exit.connect("pressed", self, "on_Exit_pressed")   # Señales del boton de exit
+
+# Recibir señal del boton de start
+func on_Start_pressed():
+	get_tree().change_scene("res://scenes/Main.tscn")
+
+# Recibir señal del boton de exit
+func on_Exit_pressed():
+	get_tree().quit()  # Cierra el juego
