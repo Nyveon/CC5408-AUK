786f17f065d57e09782f9b20978bf36ccc55cd25
Ariel Suil <ariel.suil@ug.uchile.cl>
2020-09-30 14:59:15 -0300

    added character with basic physics

---
+++ scripts/Character.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+var speed_x = 600
+var speed_y = 1000
+var friction = 0.2
+var gravity = 60
+
+var linear_vel = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	var target_vel = Input.get_action_strength("move_right") -\
+					 Input.get_action_strength("move_left")
+	
+	linear_vel.x = lerp(linear_vel.x, target_vel * speed_x, friction)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		linear_vel.y -= speed_y
+	linear_vel.y += gravity
+	
+	linear_vel = move_and_slide(linear_vel, Vector2.UP)
+	
