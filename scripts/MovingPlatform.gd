c95864378022af88bf21a39c991be6a785295890
Ariel Suil <ariel.suil@ug.uchile.cl>
2020-10-05 14:38:34 -0300

    added object selection (mouse) and time giving power (right click)

--- scripts/MovingPlatform.gd
+++ scripts/MovingPlatform.gd
@@ -14,11 +14,13 @@ func movement_y(t):
 	return 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func ready():
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	t += 1 # temporal
 	self.position = init_pos + Vector2(movement_x(t), movement_y(t))
+
+# dile al Main que este es el nodo seleccionado
+func _on_MovingPlatform_mouse_entered():
+	get_node("../../Main").selected_node = self
