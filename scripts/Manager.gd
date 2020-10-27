4c5af3929e159029c5ca033f1659515654aed7a7
Nyveon <54124297+Nyveon@users.noreply.github.com>
2020-10-27 17:03:31 -0300

    Rework del sistema quitar/dar tiempo
    modo godot

--- scripts/Manager.gd
+++ scripts/Manager.gd
@@ -3,12 +3,14 @@ extends Node
 # Declare member variables here. Examples:
 var selected_node
 var crono_power = 5 # Multiplicador para que sea un poco mas notable el poder de cronomancia
-var tiempo=100 setget set_tiempo #variable de tiempo
+var time=100.0 setget set_time #variable de time
 signal time_changed(value)
 
-func set_tiempo(value):
-	tiempo=clamp(value,0,100)
-	emit_signal("time_changed", tiempo)
+func set_time(value):
+	print(value)
+	time=clamp(value,0,100)
+	print(time)
+	emit_signal("time_changed", time)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,14 +18,20 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta): 
-	if selected_node != null:
-		if Input.get_action_strength("dar_tiempo") and tiempo>0:
-			selected_node.t += crono_power # hay que linkear a click izq/der
-			set_tiempo(tiempo-0.25)
-		elif Input.get_action_strength("quitar_tiempo") and tiempo>=0 and tiempo<100:
-			selected_node.t -= crono_power
-			set_tiempo(tiempo+0.25)
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(delta): 
+#	if selected_node != null:
+#		if Input.get_action_strength("dar_tiempo") and time>0:
+#			selected_node.t += crono_power
+#			set_time(time-0.25)
+#		if Input.get_action_strength("quitar_tiempo") and time>=0 and time<100:
+#			selected_node.t -= crono_power
+#			set_time(time+0.25)
+			
+func is_time_available():
+	return time >= 0 and time <= 100
 
+func check_time(value):
+	var new_time = time + value
+	return new_time >= 0 and new_time <= 100
 
--- scripts/MovingPlatform.gd
+++ scripts/MovingPlatform.gd
@@ -5,6 +5,10 @@ onready var init_pos = global_position
 var t = 0 setget set_t
 var periodo = 200.0
 var amplitud = 84
+var selected = false
+var time_direction = 0
+var moving = false
+
 func set_t(value):
 	t = value
 	var old_global_positionx = global_position.x
@@ -12,10 +16,6 @@ func set_t(value):
 	global_position = new_global_position
 	var delta_pos = new_global_position.x - old_global_positionx
 	$Engranaje.rotation_degrees += 3*delta_pos
-	print(delta_pos)
-
-
-# $Engranaje.rotation_degrees += t * sign(cos(t/periodo))
 
 # movimiento ida y vuelta
 func movement_x(t):
@@ -27,8 +27,35 @@ func movement_y(t):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("mouse_entered", self, "_on_mouse_entered")  # Señale de mouse
+	 # Señale de mouse
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
 
-# dile al Main que este es el nodo seleccionado
+func _physics_process(delta):
+	time_direction = Input.get_action_strength("dar_tiempo") - Input.get_action_strength("quitar_tiempo")
+	var is_pressed = Input.is_action_pressed("dar_tiempo") or Input.is_action_pressed("quitar_tiempo")
+	
+	if not Manager.check_time(-time_direction):
+		return
+	
+	if not is_pressed:
+		moving = false
+	
+	if selected or moving:
+		moving = is_pressed
+		self.t += time_direction*Manager.crono_power  # Self porque es un setter (!)
+		Manager.time -= time_direction
+	
+	
+	print("Moving: ", moving)
+	print("Selected: ", selected)
+	print("Time: ", Manager.time)
+	
+
+# Seleecionado, o no seleccionado
 func _on_mouse_entered():
-	Manager.selected_node = self
+	selected = true
+	
+func _on_mouse_exited():
+	selected = false
+
